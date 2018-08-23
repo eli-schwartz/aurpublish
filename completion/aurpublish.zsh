87e458bb2e3dafb37e26205957d9bd9bd545db8a
Joey Pabalinas <joeypabalinas@gmail.com>
2018-08-23 10:13:56 -1000

    zsh_completion: add packages to command context completion
    Signed-off-by: Joey Pabalinas <joeypabalinas@gmail.com>

--- completion/aurpublish.zsh
+++ completion/aurpublish.zsh
@@ -1,22 +1,26 @@
 #compdef aurpublish
 
 local curcontext="$curcontext" state line libs idx
-local -a pkg_dirs
+local -a pkg_dirs packages
 typeset -A descs opt_args
 
 # [keys]=commands 'vals'=description
 descs[setup]='Install githooks to the repository.'
-descs[log]='Wrap `git log` and substitute a package subtree for the revision/branch.'
+descs[log]='Wrapper for `git log`, substituting a package subtree for the revision/branch.'
+pkg_dirs=(${${(M)${(@0)$(git ls-tree -dz --name-only HEAD :/ 2>/dev/null)}:#?*}/%/\/})
 
 # leverage --help option parsing
 _arguments -C -s \
-	":COMMANDS:((setup\\:'$descs[setup]' log\\:'$descs[log]'))" \
-	":PACKAGES:->pkgs" \
+	":COMMANDS/PACKAGES:((setup\\:'$descs[setup]' log\\:'$descs[log]' $pkg_dirs[*]))" \
+	"*:PACKAGES:->pkgs" \
 	-- && ret=0
 
 # only complete package directories
 case $state in
 pkgs)
-	pkg_dirs=(${(M)${(@0)$(git ls-tree -dz --name-only HEAD :/ 2>/dev/null)}:#?*})
-	_wanted files expl 'PACKAGES' compadd -a pkg_dirs && ret=0;
+	packages=()
+	if [[ ! $words[*] =~ (^|[[:blank:]])(setup)([[:blank:]]|$) ]]; then
+		packages=($pkg_dirs)
+	fi
+	_wanted files expl 'ARGUMENTS' compadd -a packages && ret=0;
 esac
