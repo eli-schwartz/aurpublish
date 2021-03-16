archlinux/arch-install-scripts

doc: add manpage infrastructure

Makefile
@@ -9,7 +9,8 @@ BINPROGS = \
 
 BASH = bash
 
-all: $(BINPROGS)
+all: $(BINPROGS) man
+man: $(MANS)
 
 V_GEN = $(_v_GEN_$(V))
 _v_GEN_ = $(_v_GEN_0)
@@ -20,8 +21,11 @@ edit = $(V_GEN) m4 -P $@.in >$@ && chmod go-w,+x $@
 %: %.in common
 	$(edit)
 
+doc/%: doc/%.asciidoc doc/asciidoc.conf doc/footer.asciidoc
+	$(V_GEN) a2x --no-xmllint --asciidoc-opts="-f doc/asciidoc.conf" -d manpage -f manpage -D doc $<
+
 clean:
-	$(RM) $(BINPROGS)
+	$(RM) $(BINPROGS) $(MANS)
 
 check: all
 	@for f in $(BINPROGS); do bash -O extglob -n $$f; done
@@ -31,5 +35,8 @@ install: all
 	install -dm755 $(DESTDIR)$(PREFIX)/bin
 	install -m755 $(BINPROGS) $(DESTDIR)$(PREFIX)/bin
 	install -Dm644 zsh-completion $(DESTDIR)$(PREFIX)/share/zsh/site-functions/_archinstallscripts
+	for manfile in $(MANS); do \
+		install -Dm644 $$manfile -t $(DESTDIR)$(PREFIX)/share/man/man$${manfile##*.}; \
+	done;
 
 .PHONY: all clean install uninstall

eli-schwartz/aurpublish

Makefile
@@ -6,13 +6,17 @@ BASHCOMPDIR ?= $(shell pkg-config bash-completion --variable=completionsdir || e
 
 edit = sed -e 's|@HOOKSDIR@|$(HOOKSDIR)|g'
 
-all: $(NAME)
+all: $(NAME) man
+man: $(MANS)
 $(NAME): $(NAME).in
 
 %: %.in
 	@rm -f $@
 	$(edit) $< > $@
 	@test -x '$@.in' && chmod a-w,+x $@
+
+doc/%: doc/%.asciidoc doc/asciidoc.conf
+	a2x --no-xmllint --asciidoc-opts="-f doc/asciidoc.conf" -d manpage -f manpage -D doc $<
 
 install: $(NAME)
 	install -dm755 '$(DESTDIR)$(PREFIX)/bin'
@@ -23,3 +27,6 @@ install: $(NAME)
 	install -dm755 '$(DESTDIR)$(BASHCOMPDIR)'
 	install -m644 completion/$(NAME).zsh '$(DESTDIR)$(ZCOMPDIR)/_$(NAME)'
 	install -m644 completion/$(NAME).bash '$(DESTDIR)$(BASHCOMPDIR)/$(NAME)'
+	for manfile in $(MANS); do \
+		install -Dm644 $$manfile -t $(DESTDIR)$(PREFIX)/share/man/man$${manfile##*.}; \
+	done